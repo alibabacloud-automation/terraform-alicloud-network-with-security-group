56a0187006ac5d99aa84793147bfc26fb78f146e
liuyuan <709503583@qq.com>
2021-11-23 14:58:55 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/network-with-security-group"
-}
-
 locals {
   create_vpc = var.use_existing_vpc ? false : var.create_vpc
 }
